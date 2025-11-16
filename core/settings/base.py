1103b1ac4f98c52f85562749f88ad0b80607c335
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-11-16 08:10:37 +0300

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -5,7 +5,6 @@ from decouple import config, Csv
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 INSTALLED_APPS = [
-    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -13,7 +12,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 3rd-party
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
@@ -23,7 +21,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'storages',
 
-    # Local
     'apps.accounts',
     'apps.posts',
     'apps.stories',
@@ -33,21 +30,22 @@ INSTALLED_APPS = [
     'apps.hashtags',
     'apps.explore',
     'apps.notifications',
+    'apps.live',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'core.middleware.ratelimit.RateLimitMiddleware',   # <-- our rate limiter
+    'core.middleware.ratelimit.RateLimitMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
-# ---------- AUTH ----------
 AUTH_USER_MODEL = 'accounts.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
@@ -62,10 +60,9 @@ REST_FRAMEWORK = {
     },
 }
 
-# ---------- MEDIA CDN ----------
 DEFAULT_FILE_STORAGE = 'core.storages.media_cdn.CDNBucketStorage'
 AWS_S3_REGION_NAME = config('AWS_REGION')
-AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT')      # e.g. https://<id>.r2.cloudflarestorage.com
+AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_BUCKET')
@@ -73,7 +70,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = True
 AWS_QUERYSTRING_EXPIRE = 3600
 
-# ---------- CHANNELS ----------
 ASGI_APPLICATION = 'core.asgi.application'
 CHANNEL_LAYERS = {
     "default": {
@@ -82,6 +78,4 @@ CHANNEL_LAYERS = {
     },
 }
 
-# ---------- PUSH ----------
 FCM_API_KEY = config('FCM_API_KEY')
-APNS_CERT = config('APNS_CERT_PATH', default=None)
